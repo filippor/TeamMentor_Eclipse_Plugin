e05c6195fc2558d45a42368b9d1b3843f0836c5b
Dinis Cruz <dinis.cruz@owasp.org>
2013-10-25 21:30:36 +0100

    Small preferences change (to test CI)

--- TeamMentor.Eclipse.PlugIn.Fortify/src/teammentor/eclipse/plugin/fortify/preferences/PreferenceInitializer.java
+++ TeamMentor.Eclipse.PlugIn.Fortify/src/teammentor/eclipse/plugin/fortify/preferences/PreferenceInitializer.java
@@ -19,7 +19,7 @@ public class PreferenceInitializer extends AbstractPreferenceInitializer {
 		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
 		store.setDefault(PreferenceConstants.P_OPEN_ARTICLE_NEW_WINDOW, false);
 		store.setDefault(PreferenceConstants.P_TEAMMENTOR_SERVER, "https://vulnerabilities.teammentor.net");
-		store.setDefault(PreferenceConstants.P_TEAMMENTOR_SESSION_ID,"00000000-0000-0000-0000-000000000000");		
+		store.setDefault(PreferenceConstants.P_TEAMMENTOR_SESSION_ID,"00000000-0000-0000-0000-000000000001");		
 	}
 
 }
