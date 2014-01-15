664d0ed024aafca18bac9e38f4a7d2f9d5631d6b
DinisCruz-Dev <dinis.cruz+dev@owasp.org>
2014-01-15 15:01:02 +0000

    Format changes

--- TeamMentor.Eclipse.PlugIn.Fortify/src/tm/eclipse/ui/editors/StringInput.java
+++ TeamMentor.Eclipse.PlugIn.Fortify/src/tm/eclipse/ui/editors/StringInput.java
@@ -8,13 +8,13 @@ import org.eclipse.ui.IStorageEditorInput;
 public class StringInput implements IStorageEditorInput 
 {
     private IStorage storage;
-    public StringInput(IStorage storage) 					{ this.storage = storage;	}
-    public boolean				exists() 					{ return true;				}
-    public ImageDescriptor		getImageDescriptor() 		{ return null;				}
-    public String 			    getName() 					{ return storage.getName(); }
-    public IPersistableElement	getPersistable() 			{ return null;				}
-    public IStorage				getStorage() 				{ return storage; 			}
-    public String				getToolTipText() 			{ return "String-based file: " + storage.getName(); }
+    public StringInput(IStorage storage)                    { this.storage = storage;   }
+    public boolean              exists()                    { return true;              }
+    public ImageDescriptor      getImageDescriptor()        { return null;              }
+    public String               getName()                   { return storage.getName(); }
+    public IPersistableElement  getPersistable()            { return null;              }
+    public IStorage             getStorage()                { return storage;           }
+    public String               getToolTipText()            { return "String-based file: " + storage.getName(); }
     @SuppressWarnings("rawtypes")
-	public Object				getAdapter( Class adapter)  { return null; 				}
+	public Object               getAdapter( Class adapter)  { return null;              }
  }
